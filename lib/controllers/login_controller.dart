85e18f4da601cadc136c2e0101526e100991a3eb
ahsanrahman0745 <arahman@medelz.com>
2023-06-25 13:44:20 +0500

    update

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,38 +1,53 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 import 'package:http/http.dart' as http;
 
-import '../views/register.dart';
 
 class LoginController extends GetxController {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   GlobalKey<FormState> FormKey = GlobalKey();
-var AuthToken;
+
+  RxBool loading = false.obs;
+
   Future<void> login() async {
-  try{
-    var headers = {"Content-Type": "application/json"};
-    var body = {
-      "email": emailController.text.trim(),
-      "password": passwordController.text.trim()
-    };
-
-    http.Response response = await http.post(
-        Uri.parse("https://server.darjee.one/api/user/login"),
-        headers: headers,
-        body: jsonEncode(body));
-    if(response.statusCode==200){
-      var jsonData=jsonDecode(response.body);
-      print(jsonData);
-    }else{
-      print('fail');
-
+    loading.value = true;
+    await Future.delayed(Duration(seconds: 1));
+    try {
+      var headers = {"Content-Type": "application/json"};
+      var body = {
+        "email": '${emailController.text.trim()}',
+        "password": '${passwordController.text.trim()}'
+      };
+      http.Response response = await http.post(
+          Uri.parse("http://server.darjee.one/api/user/login"),
+          headers: headers,
+          body: json.encode(body));
+      print("object ${response.body}");
+      Fluttertoast.showToast(
+          msg: response.body.toString(),
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(response.body);
+        print(jsonData);
+      } else {
+        print('fail');
+      }
+      loading.value = false;
+    } catch (e) {
+      print(e);
     }
-  }catch(e){
-    print(e);
+    loading.value = false;
   }
-
-}}
+}
